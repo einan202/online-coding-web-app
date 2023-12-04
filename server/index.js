a51567c47613d26d8503326315662bab17a77c8d
einan202 <einan202@gmail.com>
2023-12-04 05:35:15 +0200

    Adding code init functionality

--- server/index.js
+++ server/index.js
@@ -24,7 +24,15 @@ const codeBlocks = [
     { id: '4', title: 'Node task', code: 'console.log("task 4");' },
   ];
 
-// Standard requests for testing server
+// Local initial code blocks data base
+const initBlocks = [
+    { id: '1', title: 'Async task', code: 'console.log("task 1");' },
+    { id: '2', title: 'Sync task', code: 'console.log("task 2");' },
+    { id: '3', title: 'React task', code: 'console.log("task 3");' },
+    { id: '4', title: 'Node task', code: 'console.log("task 4");' },
+  ];
+
+// Requests for testing the server with code blocks objects
 app.get('/', (req, res) => {
     res.json({ message: 'test' });
 });
@@ -68,52 +76,58 @@ app.put('/code-block/:id', (req, res) => {
     }
 });
 
-// Realtime socket for code block pages
+// Number of clients in each room
 let room1 = 0, room2 = 0, room3 = 0, room4 = 0;
 
+// Realtime socket for code block pages
+// Number of clients is sent in order to know if mentor or student
+// After period of 120 seconds code block will be initiated
 io.on("connection", (socket) => {
     console.log(`User id ${socket.id}`);
 
+    // When render a code block page - loading its code from data base
     socket.on("connect_1", (data) => {
         room1++;
         socket.emit("user_counter", room1);
         socket.emit(`init_${data}`, codeBlocks[data - 1]);
+        setTimeout(() => codeBlocks[0].code = initBlocks[0].code, 120000)
     });
     socket.on("connect_2", (data) => {
         room2++;
         socket.emit("user_counter", room2);
         socket.emit(`init_${data}`, codeBlocks[data - 1]);
+        setTimeout(() => codeBlocks[1].code = initBlocks[1].code, 120000)
     });
     socket.on("connect_3", (data) => {
         room3++;
         socket.emit("user_counter", room3);
         socket.emit(`init_${data}`, codeBlocks[data - 1]);
+        setTimeout(() => codeBlocks[2].code = initBlocks[2].code, 120000)
+        
     });
     socket.on("connect_4", (data) => {
         room4++;
         socket.emit("user_counter", room4);
         socket.emit(`init_${data}`, codeBlocks[data - 1]);
+        setTimeout(() => codeBlocks[3].code = initBlocks[3].code, 120000)
     });
 
+    // Update data base code from code editor element and send it to mentor
     socket.on("send_code_1", (data) => {
         socket.broadcast.emit("receive_code_1", data); 
         codeBlocks[0].code = data.code;
-        // console.log(codeBlocks[0]);
     });
     socket.on("send_code_2", (data) => {
         socket.broadcast.emit("receive_code_2", data); 
         codeBlocks[1].code = data.code;
-        // console.log(codeBlocks[1]);
     });
     socket.on("send_code_3", (data) => {
         socket.broadcast.emit("receive_code_3", data); 
         codeBlocks[2].code = data.code;
-        // console.log(codeBlocks[2]);
     });
     socket.on("send_code_4", (data) => {
         socket.broadcast.emit("receive_code_4", data); 
         codeBlocks[3].code = data.code;
-        // console.log(codeBlocks[3]);
     });
 });
 
